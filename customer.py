c5a7a87469d1f45b1c22a116ed11c93dc6a4fbe0
Yehuda Abramczyk <laiby666@gmail.com>
2024-02-12 13:57:59 +0200

    Adding file customer.py
    adding file for customer code

---
+++ customer.py
@@ -0,0 +1,8 @@
+class Customer:
+    def __init__(self, first_name, last_name, id, phone, debt, date) -> None:
+        self._first_name = first_name
+        self._last_name = last_name
+        self._id = id
+        self._phone = phone
+        
+        
