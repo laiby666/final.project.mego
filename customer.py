3474bcc1d565c3c1b63df16b95b87faa97211ddc
Yehuda Abramczyk <laiby666@gmail.com>
2024-02-15 14:10:52 +0200

    Started the code for client
    adding class for customer data

--- customer.py
+++ customer.py
@@ -4,5 +4,22 @@ class Customer:
         self._last_name = last_name
         self._id = id
         self._phone = phone
-        
-        
+        self._debt = debt
+        self._date = date
+
+    @property
+    def id(self):
+        return self._id
+    
+    @property
+    def debt(self):
+        return self._debt
+    @debt.setter
+    def add_debt(self, debt):
+        if type(debt) is not int:
+            print("Error: debt is not an int")
+            return
+        self._debt += debt
+
+    def __str__(self):
+        return f"name: {self._first_name} {self._last_name}, id: {self._id}, debt: {self._debt}"
