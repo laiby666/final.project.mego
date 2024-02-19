2c4738cfe50be549f2406348f17af5b4135099a3
Yehuda Abramczyk <laiby666@gmail.com>
2024-02-19 14:58:51 +0200

    Adding socket and Threads
    add a server socket to the server code,
    and write code for open a new thread for every client

--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 import sys
 import os
-from customer import Customer
+from customer import Customer, CustomerActions
+import threading
+import socket
+
+host = '127.0.0.1'
+port = 12345 
+
 if len(sys.argv) < 2:
     print("Error: missing csv file name!")
     quit()
@@ -9,6 +15,7 @@ csv_file = sys.argv[1]
 if not os.path.exists(csv_file):
     with open(csv_file, "w"):
         pass
+
 
 customers = []
 with open(csv_file, "r") as fd:
@@ -30,5 +37,21 @@ for customer in customers:
 while True:
     query = input("==> ")
     if query == "quit":
-        print("Bye...")
+        print("Bye")
         break
+
+def choose_action(client_sock):
+    while True:
+        command = client_sock.recv(2048).split(" ")
+        if command[0] == "select":
+            customer.select(command)
+        if command[0] == "set":
+            customer.set(command)
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((host, port))
+server_socket.listen(40)
+while True:
+    client_socket, client_address = server_socket.accept()
+    t = threading.Thread(target=choose_action, args=(client_socket,))
+    t.start()
