487f1e4c572cd3047eaaff2f766ce3ded35d7ccd
Yehuda Abramczyk <laiby666@gmail.com>
2024-02-20 14:55:29 +0200

    Add funcion of commands
    add a few funcion:
    search and found- for searching any of Customer variables
    debt_less_or_more_than
    date_before_or_after_this

--- commands.py
+++ commands.py
@@ -1,7 +1,7 @@
 from customer import Customer
 
 class Commands:
-    def search(self, command:str):       
+    def search(self:Customer, command:str):       
         if command.startswith("select first name"):
             return self.first_name               
         if command.startswith("select last name"):
@@ -15,23 +15,49 @@ class Commands:
         if command.startswith("select date="):
             return self.date
     
-    def select(command, customers:list[Customer]):
-        found = [] 
+    def found(command:str, customers:list[Customer]):
+        found = 0 
         for customer in customers:
             search = Commands.search(customer, command) 
             if search in command:
-                found.append(customer)
+                found += 1
                 print(customer)
         if not found:
             print("Not found") 
-                   
+
+    def debt_less_or_more_than(command:str, customers:list[Customer]):
+        found = 0
+        point = float(command.isdigit())
+        for customer in customers:
+            if "<" in command:
+                if customer.debt > point:
+                    print(customer)
+                    found += 1
+            elif ">" in command:
+                if customer.debt < point:
+                    print(customer)
+                    found += 1
+        if not found:
+            print("Not found")
+    
+    def date_before_or_after_this(command:str, customers:list[Customer]):
+        pass
+
+    def select(command:str, customers:list[Customer]):
+        if command.startswith(("select debt<","select debt>")):
+            Commands.debt_less_or_more_than(command, customers)
+        elif command.startswith(("select date<","select date>")):
+            Commands.date_before_or_after_this(command, customers)
+        else:
+            Commands.found(command, customers)
+
     def set(command, customers):
         pass
 
 li = [Customer(*"Moshe,Cohen,12345678,0501234567,-45,12/02/2024".split(",")),
-Customer(*"Avraham,Levi,12345644,0501234555,-45,12/02/2024".split(",")),
-Customer(*"Meir,Reich,12345655,0501234444,-45,12/02/2024".split(",")),
-Customer(*"Noach,Paloch,12345666,0501234566,-45,12/02/2024".split(",")),
+Customer(*"Avraham,Levi,12345644,0501234555,300,12/02/2024".split(",")),
+Customer(*"Meir,Reich,12345655,0501234444,-500,12/02/2024".split(",")),
+Customer(*"Noach,Paloch,12345666,0501234566,-20,12/02/2024".split(",")),
 Customer(*"Moshe,Cohen,12345678,0501234567,45,13/02/2024".split(","))]
-command = "select first name=Meir"
+command = "select debt<10"
 Commands.select(command, li)
