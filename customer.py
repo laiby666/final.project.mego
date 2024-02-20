4ebaf67a9a044e3e799b65e5971923d4bce580ff
Yehuda Abramczyk <laiby666@gmail.com>
2024-02-20 15:03:17 +0200

    Add property. Out funcion
    adding property for all the Customer parameters
    out funcion of actions to commands file

--- customer.py
+++ customer.py
@@ -4,16 +4,27 @@ class Customer:
         self._last_name = last_name
         self._id = id
         self._phone = phone
-        self._debt = int(debt)
+        self._debt = float(debt)
         self._date = date
 
     @property
+    def first_name(self):
+        return self._first_name
+    @property
+    def last_name(self):
+        return self._last_name
+    @property
     def id(self):
         return self._id
-    
+    @property
+    def phone(self):
+        return self._phone    
     @property
     def debt(self):
         return self._debt
+    @property
+    def date(self):
+        return self._date
     
     def add_debt(self, debt):
         if type(debt) is not int:
@@ -22,4 +33,4 @@ class Customer:
         self._debt += debt
 
     def __str__(self):
-        return f"name: {self._first_name} {self._last_name}, id: {self._id}, debt: {self._debt}"
+        return f"name: {self._first_name} {self._last_name}, id: {self._id}, phone: {self.phone} debt: {self._debt}"
